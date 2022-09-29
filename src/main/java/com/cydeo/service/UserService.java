9938fd36450e72936c8e8ff2e60827197d84e702
Vasyl <vasyl.b.dobrianski@gmail.com>
2022-09-29 14:31:18 -0500

    CRUD implemented

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -4,11 +4,8 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends CrudService<UserDTO,String> {
 
-    UserDTO save(UserDTO user);
-    UserDTO findById(String username);
-    List<UserDTO> findAll();
-    void deleteById(String username);
+
 
 }
